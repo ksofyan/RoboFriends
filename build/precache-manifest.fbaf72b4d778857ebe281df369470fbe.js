21f1b0a7280769a93dfcf94f4f017109a73b59f0
Karina Ramadhan <carinasofyan@gmail.com>
2019-02-09 16:26:10 +0800

    adding project file

---
+++ build/precache-manifest.fbaf72b4d778857ebe281df369470fbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "14571714e56f8edd39ad",
+    "url": "/static/js/main.14571714.chunk.js"
+  },
+  {
+    "revision": "e030b02bf78e65ce2a62",
+    "url": "/static/js/1.e030b02b.chunk.js"
+  },
+  {
+    "revision": "14571714e56f8edd39ad",
+    "url": "/static/css/main.71c52589.chunk.css"
+  },
+  {
+    "revision": "e030b02bf78e65ce2a62",
+    "url": "/static/css/1.b5c1e391.chunk.css"
+  },
+  {
+    "revision": "2d70e2f4eba8334432a6798b6a732e7f",
+    "url": "/index.html"
+  }
+];

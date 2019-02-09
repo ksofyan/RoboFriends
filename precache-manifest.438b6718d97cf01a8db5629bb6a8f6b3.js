834e5d338bb9b1ba17268ae83c75b17cb1c09ebc
Karina Ramadhan <carinasofyan@gmail.com>
2019-02-09 17:20:05 +0800

    Updates

---
+++ precache-manifest.438b6718d97cf01a8db5629bb6a8f6b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "34a0df219a896a414105",
+    "url": "/robofriends/static/js/main.34a0df21.chunk.js"
+  },
+  {
+    "revision": "7348fbf3b507b88bca42",
+    "url": "/robofriends/static/js/1.7348fbf3.chunk.js"
+  },
+  {
+    "revision": "34a0df219a896a414105",
+    "url": "/robofriends/static/css/main.b821f3f5.chunk.css"
+  },
+  {
+    "revision": "7348fbf3b507b88bca42",
+    "url": "/robofriends/static/css/1.b0412f3f.chunk.css"
+  },
+  {
+    "revision": "cdb8cc8abd3fbc3d510a7f7e3f0ec09f",
+    "url": "/robofriends/index.html"
+  }
+];
